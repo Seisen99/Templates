31079dea08804e86e104f294bffc74f946afb7d1
Philippe Francisco <lpfrancisco99@gmail.com>
2022-12-16 21:56:22 +0100

    cleanbord

--- fancy_template.rb
+++ fancy_template.rb
@@ -170,6 +170,10 @@ after_bundle do
   file 'app/controllers/application_controller.rb', <<~RUBY
     class ApplicationController < ActionController::Base
       before_action :authenticate_user!
+
+      def after_sign_out_path_for(_resource_or_scope)
+        new_user_session_path
+      end
     end
   RUBY
 
